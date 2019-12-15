8add9f67fd99c46481409f3fbb2fe7a2e07631fb
Anna Kubiak <ainakubiak@gmail.com>
2019-12-15 18:28:24 +0100

    finish watch

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+function odliczanie(){
+let watchBox = document.querySelector('#watch')
+let watch = new Date();
+let hour = watch.getHours();
+let minutes = watch.getMinutes();
+    minutes<10? minutes =`0${minutes}`:minutes
+let seconds = watch.getSeconds();
+    seconds<10? seconds =`0${seconds}`:seconds
+
+watchBox.innerHTML = `${hour}:${minutes}:${seconds}`;
+setTimeout('odliczanie()', 1000);
+}
+
+window.addEventListener("load", odliczanie);
